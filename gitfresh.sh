d7a762a7a6f372debd229e8dbabb20c00374507b
Novarizark <novarizark@gmail.com>
2022-08-03 15:17:50 +0800

    refresh

--- gitfresh.sh
+++
@@ -1,4 +0,0 @@
-git add conf/* lib utils db
-git add ReadMe.md *.py *.sh
-git commit -m "refresh"
-git push 
--- run_watchdog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-source ~/njord/bashrc_njord
-cd /home/pathop/njord/workspace/calypso_pipeline/
-
-# spin-up run if needed
-python3 ./calypso_wrf_watchdog.py
